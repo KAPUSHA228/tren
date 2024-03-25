077953cb0902ae45b793e05998397ca1c31e637c
KAPUSHA228 <125039835+KAPUSHA228@users.noreply.github.com>
2024-03-26 00:47:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,22 +10,22 @@ int main() {
 	vec[0] = a;
 	vec[1] = b;
 	TPolinom polinom(vec);
-	cout << "Исходный полином: " << polinom.ToString() << endl;
+	cout << "Исходный полином polinom: " << polinom.ToString() << endl;
+	cout << "polinom после дифференцирования: " << polinom.Div(1,1).ToString() << endl;
+	cout << "polinom после интегрирования: " << polinom.Integral(1, 1).ToString() << endl;
 	TPolinom p = polinom;
-	cout << "Его двойник: " << polinom.ToString() << endl;
+	cout << "Его двойник р: " << polinom.ToString() << endl;
 	//p=p.MultMonom(a);
 	//cout <<"Полином p после умножения на моном а: " << p.ToString() << endl;
 	//cout << (p == polinom);
 	//p.AddMonom(b);
 	//p.AddMonom(a);
-	cout <<"Сложение П: "<<(polinom + p).ToString();
-	cout<<"Умножение П: "<<(polinom * p).ToString();
+	cout <<"Сложение polinom + p: "<<(polinom + p).ToString() << endl;
+	cout<<"Умножение polinom * p: "<<(polinom * p).ToString() << endl;
 	polinom.AddMonom(c);
-	cout << "Добавив 3x1^2х2^3х3^4 : " << polinom.ToString() << endl;
+	cout << "Добавив к polinom 3x1^2х2^3х3^4 : " << polinom.ToString() << endl;
     	polinom.AddMonom(t);
-	cout << "Добавив 5x1^1х2^2х3^1 : " << polinom.ToString() << endl;
-	cout << " " << (polinom*3).ToString() << endl;
-	cout << " " <<polinom.ToString() << endl;
- 
-	
+	cout << "Добавив к polinom 5x1^1х2^2х3^1 : " << polinom.ToString() << endl;
+	cout << "Умножение polinom на константу k=3: " << (polinom*3).ToString() << endl;
+	cout << "Исходный полином polinom для самопроверки: " <<polinom.ToString() << endl;
 }
